2fb5ec3d7ad8057ab836378d914a7e61af42023d
sam-dev-95 <wlodawskicodes@gmail.com>
2023-01-09 09:59:09 -0500

    Finish problem sovling

--- problem-solving/challenge-1/index.ts
+++ problem-solving/challenge-1/index.ts
@@ -1,1 +1,24 @@
-export const numbersFractionCalculator = (numbers: number[]) => {};
+export const numbersFractionCalculator = (numbers: number[]) => {
+
+    let positives = 0;
+    let negatives = 0;
+    let zeros = 0;
+
+    numbers.forEach(num => {
+        if (num > 0) {
+            positives += 1;
+        } else if (num < 0) {
+            negatives += 1;
+        } else {
+            zeros += 1;
+        }
+    });
+
+    const total = positives + negatives + zeros;
+
+    return {
+        positives: (positives / total).toFixed(6),
+        negative: (negatives / total).toFixed(6),
+        zeros: (zeros / total).toFixed(6),
+    }
+};
--- problem-solving/challenge-2/index.ts
+++ problem-solving/challenge-2/index.ts
@@ -3,5 +3,25 @@ export const diceFacesCalculator = (
   dice2: number,
   dice3: number
 ): number => {
-  return 0;
+  let max = 0;
+
+  let diceArr: any = [];
+
+  [dice1, dice2, dice3].forEach(item => {
+    if (item <=0 || item > 6) {
+      throw new Error('Dice out of number range');
+    }
+
+    if (!diceArr.includes(item)) {
+      diceArr.push(item);
+
+      if (max < item) {
+        max = item;
+      }
+    } else {
+      max = item;
+    }
+  })
+
+  return max * (4 - diceArr.length);
 };
